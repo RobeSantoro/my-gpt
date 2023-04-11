b9f70237e2a05f8366da0dbc8cc20472913bcba8
RobeSantoro <santoro.robe@gmail.com>
2023-04-11 10:13:40 +0200

    Add daisyUI

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -6,30 +6,62 @@ import send from './assets/send.svg'
 function App() {
 
   return (
+    <div className="drawer drawer-mobile">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="flex flex-col items-center justify-center drawer-content">
+        {/* <!-- Page content here --> */}
+        <label htmlFor="my-drawer" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
-    <div id='app'>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer" className="drawer-overlay"></label>
+        <ul className="p-4 menu w-80 bg-base-100 text-base-content">
+          {/* <!-- Sidebar content here --> */}
+          <li><a>Sidebar Item 1</a></li>
+          <li><a>Sidebar Item 2</a></li>
+        </ul>
 
-      <aside className="w-1/3 h-screen p-1 bg-slate-950">
-        <h2 className="text-xs">Chat History</h2>
-      </aside>
-      
-      <section className="w-2/3 text-center" >
-        <h1 className="p-2 text-xl" >My GPT</h1>
-        <div id="chat_container"></div>
-        <form action="">
-          <textarea
-            name="message"
-            cols="30"
-            rows="10"
-            placeholder="Ctrl + Enter to send...">
-          </textarea>
-          <button>
-            <img src={send} alt='send' />
-          </button>
-        </form>
-      </section>
+      </div>
     </div>
+
   )
 }
 
 export default App
+
+/* 
+
+<div className="drawer">
+
+<section className="drawer-content">
+
+  <h1 className="p-2 text-xl" >My GPT</h1>
+  <div id="chat_container"></div>
+  <form action="">
+    <textarea
+      name="message"
+      cols="30"
+      rows="10"
+      placeholder="Ctrl + Enter to send...">
+    </textarea>
+    <button className="btn">
+      <img src={send} alt='send' />Send
+    </button>
+  </form>
+
+  <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+</section>
+
+<aside className="drawer-side">
+
+  <label htmlFor="my-drawer" className="drawer-overlay"></label>
+  
+  <h2 className="text-xs">Chat History</h2>
+  <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+  <ul className="p-4 menu w-80 bg-base-100 text-base-content">
+    <li><a>Sidebar Item 1</a></li>
+    <li><a>Sidebar Item 2</a></li>
+  </ul>
+</aside>
+
+</div> */
