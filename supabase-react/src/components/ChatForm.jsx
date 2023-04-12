90360a32413839850fc475b1ab30f76b1f6f7c1c
RobeSantoro <santoro.robe@gmail.com>
2023-04-13 01:05:30 +0200

    Update Server Address

--- supabase-react/src/components/ChatForm.jsx
+++ supabase-react/src/components/ChatForm.jsx
@@ -22,7 +22,7 @@ function ChatForm({ messages, setMessages }) {
 
     // Fetch chat response and set messages
     try {
-      const response = await fetch('http://localhost:5000', {
+      const response = await fetch('https://my-gpt-3s17.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
