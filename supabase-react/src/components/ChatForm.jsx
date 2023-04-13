7a0f6020e60d57115ee7ecc8b155925a32c759e0
RobeSantoro <santoro.robe@gmail.com>
2023-04-13 17:42:06 +0200

    Rollback

--- supabase-react/src/components/ChatForm.jsx
+++ supabase-react/src/components/ChatForm.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
 
-const isDebug = import.meta.env.DEBUG
-const renderComUrl = import.meta.env.RENDER_COM_URL
-const localhost = import.meta.env.LOCALHOST_URL
-
 function ChatForm({ messages, setMessages }) {
   const [messageInput, setMessageInput] = useState('');
 
@@ -11,8 +7,6 @@ function ChatForm({ messages, setMessages }) {
     e.preventDefault();
 
     if (!messageInput.trim()) return;
-
-    // user's chatstripe
 
     const updatedMessages = [
       { role: 'system', content: 'You are a helpful assistant, your name is GPT-4. You say "Sir" at the beginning of every answer.' },
@@ -24,16 +18,9 @@ function ChatForm({ messages, setMessages }) {
     // clear the textarea input
     setMessageInput('');
 
-    console.log('isDebug', isDebug);
-    console.log('renderComUrl', renderComUrl);
-    console.log('localhost', localhost);
-
-    url_to_fetch = `${isDebug ? localhost : renderComUrl}`
-    console.log('url_to_fetch', url_to_fetch);
-
     // Fetch chat response and set messages
     try {
-      const response = await fetch(url_to_fetch, {
+      const response = await fetch('https://my-gpt-3s17.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -64,24 +51,24 @@ function ChatForm({ messages, setMessages }) {
 
   return (
 
-    <form
-      onSubmit={handleSubmit}
-      className="box-border"
-    >
-      <div className="absolute bottom-0 w-full ">
-        <div className='flex justify-between w-full'>
-          <textarea
-            className="w-full h-24 m-1 text-black bg-white textarea textarea-secondary"
-            name="message"
-            value={messageInput}
-            onChange={(e) => setMessageInput(e.target.value)}
-            onKeyDown={handleKeyDown}
-            placeholder="Ctrl + Enter to send..."
-          />
-          <button type="submit" className="h-auto m-1 btn btn-primary">Invia</button>
+      <form
+        onSubmit={handleSubmit}
+        className="box-border"
+      >
+        <div className="absolute bottom-0 w-full ">
+          <div className='flex justify-between w-full'>
+            <textarea
+              className="w-full h-24 m-1 text-black bg-white textarea textarea-secondary"
+              name="message"
+              value={messageInput}
+              onChange={(e) => setMessageInput(e.target.value)}
+              onKeyDown={handleKeyDown}
+              placeholder="Ctrl + Enter to send..."
+            />
+            <button type="submit" className="h-auto m-1 btn btn-primary">Invia</button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
 
   );
 }
