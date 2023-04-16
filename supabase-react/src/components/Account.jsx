df8b4e021a99478d533aae4cff00bfaa27b88122
RobeSantoro <santoro.robe@gmail.com>
2023-04-17 00:11:30 +0200

    Add editable conversationTitle

--- supabase-react/src/components/Account.jsx
+++ supabase-react/src/components/Account.jsx
@@ -6,12 +6,8 @@ import Chat from './Chat'
 
 function Account({ session }) {
 
-  const [conversationTitle, setconversationTitle] = useState('Mio GPT')
-
-  const loadLastChatMessages = () => {
-    const storedMessages = localStorage.getItem('messages');
-    return storedMessages ? JSON.parse(storedMessages) : [];
-  };
+  const [conversationTitle, setConversationTitle] = useState('Mio GPT')
+  const [isEditingTitle, setIsEditingTitle] = useState(false);
 
   const createNewConversation = () => {
     localStorage.removeItem('messages');
@@ -58,6 +54,11 @@ function Account({ session }) {
     }, 100);
 
   }
+
+  const loadLastChatMessages = () => {
+    const storedMessages = localStorage.getItem('messages');
+    return storedMessages ? JSON.parse(storedMessages) : [];
+  };
 
   const [lastMessages, setLastMessages] = useState(loadLastChatMessages());
 
@@ -116,6 +117,20 @@ function Account({ session }) {
     setLoading(false)
   }
 
+  const handleTitleClick = () => {
+    setIsEditingTitle(true);
+  };
+
+  const handleTitleChange = (event) => {
+    setConversationTitle(event.target.value);
+  };
+
+  const handleTitleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setIsEditingTitle(false);
+    }
+  };
+
   return (
     <div className="container flex flex-1 bg-base-100">
 
@@ -142,13 +157,30 @@ function Account({ session }) {
 
               <li onClick={saveJSONConversation}>
                 <a>Salva Conversazione</a></li>
+
               {/* <li><a>Informazioni Privacy</a></li> */}
             </ul>
           </div>
 
           {/* CENTERD TITLE */}
           <div className="flex-1 place-content-center">
-            <h2 className="text-2xl normal-case ">{conversationTitle}</h2>
+            {isEditingTitle ? (
+              <input
+                type="text"
+                value={conversationTitle}
+                onChange={handleTitleChange}
+                onKeyDown={handleTitleKeyDown}
+                placeholder="Titolo Conversazione"
+                className="w-full max-w-xs input input-bordered"
+              />
+            ) : (
+              <h2
+                className="text-2xl normal-case cursor-pointer"
+                onClick={handleTitleClick}
+              >
+                {conversationTitle}
+              </h2>
+            )}
           </div>
 
           {/* RIGHT MENU */}
@@ -281,54 +313,4 @@ function Account({ session }) {
   )
 }
 
-export default Account;
-
-
-{/* SHOPPING CART */ }
-{/* <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <div className="indicator">
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                <span className="badge badge-sm indicator-item">8</span>
-              </div>
-            </label>
-            <div tabIndex={0} className="mt-3 shadow card card-compact dropdown-content w-52 bg-base-100">
-              <div className="card-body">
-                <span className="text-lg font-bold">8 Items</span>
-                <span className="text-info">Subtotal: $999</span>
-                <div className="card-actions">
-                  <button className="btn btn-primary btn-block">View cart</button>
-                </div>
-              </div>
-            </div>
-          </div> */}
-
-{/* BIG BUTTONS MODAL */ }
-{/* <input type="checkbox" id="big-buttons-modal" className="modal-toggle" />
-        <div className='modal' >
-          <ul tabIndex={0} className="flex flex-col justify-center gap-3 px-10 ">
-            <li className='w-full btn btn-primary' >Nuova Conversazione</li>
-            <li className='w-full btn' >Conversazioni Salvate</li>
-            <li className='w-full btn btn-accent' >Informarmazioni Privacy</li>
-          </ul>
-        </div> */}
-
-
-
-{/* FOOTER
-        <div className="flex justify-between px-20 pb-1">
-
-          <button className='flex flex-col items-center'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-            <span className="btm-nav-label">Home</span>
-          </button>
-          <button className='flex flex-col items-center'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            <span className="btm-nav-label">Warnings</span>
-          </button>
-          <button className='flex flex-col items-center'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
-            <span className="btm-nav-label">Statics</span>
-          </button>
-
-        </div> */}
+export default Account
